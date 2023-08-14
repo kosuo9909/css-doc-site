7409e59fda9a6260d886d2dc494208e851a47eb2
Kostadin Stanev <kostadin.stanev@p3-group.com>
2023-08-14 12:25:13 +0300

    initial commit to continue home

---
+++ src/components/LeftNavBar.jsx
@@ -0,0 +1,15 @@
+const LeftSideBar = ({ info }) => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default LeftSideBar;
