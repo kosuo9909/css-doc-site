ab470723552a90aab66df242acba044450f59deb
Kostadin Stanev <kostadin.stanev@p3-group.com>
2023-08-14 12:40:11 +0300

    initial commit to continue home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import LeftSideBar from './components/LeftNavBar';
+import { mockNavBar } from './components/storage/mockNavBar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LeftSideBar mockNavBar={mockNavBar} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/LeftNavBar.jsx
+++ src/components/LeftNavBar.jsx
@@ -1,14 +1,19 @@
-const LeftSideBar = ({ info }) => {
+import './LeftNavBar.css';
+const LeftSideBar = ({ mockNavBar }) => {
+  console.log(mockNavBar[0]);
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <a href=""></a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      {Object.keys(mockNavBar).map((key) => (
+        <nav key={key} className="left-navbar">
+          <h6 className="bold-red">{Object.keys(mockNavBar[key])}</h6>
+          <ul>
+            <li>
+              <a href="/">{Object.values(mockNavBar[key])}</a>
+            </li>
+          </ul>
+        </nav>
+      ))}
+    </>
   );
 };
 
